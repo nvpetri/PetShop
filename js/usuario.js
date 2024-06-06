0ab756076529894ec2849bdc2c1f16e676a39ff3
oghenrique <santosguhenrique@gmail.com>
2024-06-05 21:55:49 -0300

    erro no login

---
+++ js/usuario.js
@@ -0,0 +1,13 @@
+export async function loginUsuario(usuario) {
+    const url = 'http://localhost:8080/v1/petshop/usuario/login';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    };
+
+    const response = await fetch(url, options);
+    return response.ok;
+}
