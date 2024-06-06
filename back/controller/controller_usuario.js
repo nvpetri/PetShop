e15009cd6256269465dfc5ac28db0c7c7594be34
Gustavo <santosguhenrique@gmail.com>
2024-06-06 16:37:54 -0300

    indo

--- back/controller/controller_usuario.js
+++ back/controller/controller_usuario.js
@@ -10,10 +10,7 @@ const insertUser = async function(dadosCliente, content) {
             if (!dadosUsuario.nome || dadosUsuario.nome.length === 0 || dadosUsuario.nome.length > 40 ||
                 !dadosUsuario.email || dadosUsuario.email.length === 0 || dadosUsuario.email.length > 255 ||
                 !dadosUsuario.senha || dadosUsuario.senha.length === 0 || dadosUsuario.senha.length > 80 ||
-                (dadosUsuario.telefone && dadosUsuario.telefone.length > 14) ||
-                (dadosUsuario.data_nascimento && !isValidDate(dadosUsuario.data_nascimento)) ||
-                (dadosUsuario.foto_perfil && dadosUsuario.foto_perfil.length > 65535) ||
-                isNaN(dadosUsuario.id_sexo)
+                (dadosUsuario.telefone && dadosUsuario.telefone.length > 14)
             ) {
                 return ERROR_Messages.ERROR_REQUIRED_FIELDS;
             } else {
