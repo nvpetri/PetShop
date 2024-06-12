eb2a88d39a9dfc2ddc1248332557f130f717ff5e
nvpetri <nicolaspetri18@gmail.com>
2024-06-11 22:14:31 -0300

    refactor

--- cms/js/sexo.js
+++
@@ -1,9 +0,0 @@
-export async function getSexos() {
-    const url = 'http://localhost:8080/v1/petshop/sexo'
-
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
-
-    return data.sexo
-}
